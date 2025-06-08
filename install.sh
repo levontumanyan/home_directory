0ef3f98673118d3658b4aa7e0088d18900560478
levont <5532305+levontumanyan@users.noreply.github.com>
2025-06-08 18:31:36 -0400

    add one common dir for backups

--- install.sh
+++ install.sh
@@ -8,19 +8,23 @@ DOTFILES_DIR="$HOME/home_directory"
 # download the repo
 # rm -rf $DOTFILES_DIR/.git
 
-BACKUP_DIR="$HOME/dotfiles_backup_$(date +%d_%m_%Y_%H:%M:%S)"
+# dated backup dir
+BACKUP_DIR="$HOME/dotfiles_backup/$(date +%d_%m_%Y_%H:%M:%S)"
 
 # List your dotfiles here
 DOTFILES=".zshrc env.zsh completions.zsh aliases.zsh"
 
-echo "Backing up existing dotfiles to $BACKUP_DIR"
-mkdir -p "$BACKUP_DIR"
+# create general backup dir
+[ ! -d "$HOME/dotfiles_backup/" ] && mkdir -pv "$HOME/dotfiles_backup/"
+# create dated backup dir
+mkdir -pv "$BACKUP_DIR"
 
+echo "Backing up existing dotfiles to $BACKUP_DIR"
 for file in $DOTFILES; do
   if [ -f "$HOME/$file" ]; then
-    mv "$HOME/$file" "$BACKUP_DIR/"
+    mv -v "$HOME/$file" "$BACKUP_DIR/"
   fi
-  ln -sfn "$DOTFILES_DIR/$file" "$HOME/$file"
+  ln -sfnv "$DOTFILES_DIR/$file" "$HOME/$file"
 done
 
 # move bash related files and .profile out to the backup dir if they exist 
