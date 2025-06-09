7cf833c95e5351c04253f83c8f7533e834dfe344
levont <5532305+levontumanyan@users.noreply.github.com>
2025-06-08 20:47:52 -0400

    fix files env variable

--- uninstall.sh
+++ uninstall.sh
@@ -4,9 +4,10 @@ set -euox
 
 # locate the backup dir
 BACKUP_DATE_DIR=$(ls -dt "$HOME"/dotfiles_backup_* 2>/dev/null | head -n 1)
+DOTFILES=".zshrc env.zsh completions.zsh aliases.zsh"
 
 echo "Removing dotfile symlinks from \$HOME..."
-for file in $FILES; do
+for file in $DOTFILES; do
 	if [ -L "$HOME/$file" ] && [ "$(readlink "$HOME/$file")" = "$DOTFILES_DIR/$file" ]; then
 	rm "$HOME/$file"
 	echo "Removed $HOME/$file"
@@ -15,7 +16,7 @@ done
 
 if [ -n "$BACKUP_DATE_DIR" ]; then
 	echo "Restoring backups from $BACKUP_DATE_DIR..."
-	for file in $FILES; do
+	for file in $DOTFILES; do
 		if [ -f "$BACKUP_DATE_DIR/$file" ]; then
 			mv "$BACKUP_DATE_DIR/$file" "$HOME/$file"
 			echo "Restored $HOME/$file"
