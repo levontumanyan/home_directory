7d1713f1447c597aad804eb37b0b02653ba8c2d6
levont <5532305+levontumanyan@users.noreply.github.com>
2025-06-08 20:56:47 -0400

    add setup envs in one files and source

--- setup_envs.sh
+++ setup_envs.sh
@@ -1,3 +1,4 @@
 DOTFILES_DIR="$HOME/home_directory"
 BACKUP_DATE_DIR=$(ls -dt "$HOME"/dotfiles_backup_* 2>/dev/null | head -n 1)
 DOTFILES=".zshrc env.zsh completions.zsh aliases.zsh"
+BACKUP_DIR="$HOME/dotfiles_backup/$(date +%d_%m_%Y_%H:%M:%S)"
