ad46fa6002bb53a194031d381619e8b573ee9f5c
levont <5532305+levontumanyan@users.noreply.github.com>
2025-09-21 21:31:06 -0400

    add sudo to be an alias of doas

--- dotfiles/aliases.zsh
+++ dotfiles/aliases.zsh
@@ -3,3 +3,11 @@ alias kcd="kubectl config set-context --current --namespace"
 
 alias python="python3"
 alias pip="pip3"
+
+sudo() {
+    if command -v doas >/dev/null 2>&1; then
+        doas "$@"
+    else
+        command sudo "$@"
+    fi
+}
