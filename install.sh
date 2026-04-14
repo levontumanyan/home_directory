17743f6d3de004651a198f2c6d093e689278db41
levontumanyan <5532305+levontumanyan@users.noreply.github.com>
2026-04-13 21:57:08 -0400

    better backup mechanism

--- install.sh
+++ install.sh
@@ -11,25 +11,22 @@ set -euox
 # create general backup dir
 [ ! -d "$BACKUP_DIR" ] && mkdir -pv "$BACKUP_DIR"
 
-# backup some files that could be there
-for file in "$HOME"/.zsh* "$HOME"/.zprofile "$HOME"/.bash* "$HOME"/.profile; do
-  [ -e "$file" ] && mv -v "$file" "$BACKUP_DIR/"
-done
-
 # backup ohmyzsh
 if [ -d "$HOME/.oh-my-zsh" ] && [ ! -d "$BACKUP_DIR/.oh-my-zsh" ]; then
   mv -v "$HOME/.oh-my-zsh" "$BACKUP_DIR/"
 fi
 
+# backup any existing files that will be overwritten by symlinks
 echo "Backing up existing dotfiles to $BACKUP_DIR"
-for file in $BACKUP_FILES; do
-  if [ -e "$HOME/$file" ]; then
-    mv -v "$HOME/$file" "$BACKUP_DIR/"
+find "$DOTFILES_DIR/dotfiles" -type f | while read -r dot_file; do
+  target="$HOME/$(basename "$dot_file")"
+  if [ -e "$target" ] && [ ! -L "$target" ]; then
+    mv -v "$target" "$BACKUP_DIR/"
   fi
 done
 
 # find all the dotfiles in the corresponding directory and symlink
-find $DOTFILES_DIR/dotfiles -type f | while read -r dot_file; do
+find "$DOTFILES_DIR/dotfiles" -type f | while read -r dot_file; do
   echo "linking dotfile: $dot_file"
   ln -sfnv "$dot_file" "$HOME/$(basename "$dot_file")"
 done
