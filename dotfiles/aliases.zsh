8edbb4a326ee85e39c2584f03b86c2204ae2dd74
levont <5532305+levontumanyan@users.noreply.github.com>
2026-04-16 22:04:39 -0400

    add fkill fzf

--- dotfiles/aliases.zsh
+++ dotfiles/aliases.zsh
@@ -6,9 +6,27 @@ alias pip="pip3"
 
 # on machines that have only doas
 sudo() {
-    if command -v doas >/dev/null 2>&1; then
-        doas "$@"
-    else
-        command sudo "$@"
-    fi
+	if command -v doas >/dev/null 2>&1; then
+		doas "$@"
+	else
+		command sudo "$@"
+	fi
+}
+
+fkill() {
+	selection=$(ps -eo pid,ppid,%cpu,%mem,etime,cmd 2>/dev/null \
+	|| ps -axww -o pid,ppid,%cpu,%mem,etime,command)
+
+	selection=$(echo "$selection" | sed 1d | sort -k3 -nr \
+	| fzf -m --header="Kill process")
+
+	[ -z "$selection" ] && return
+
+	pids=$(echo "$selection" | awk '{print $1}')
+
+	for pid in $pids; do
+		if kill -15 $pid 2>/dev/null; then
+			echo "Killed: $pid"
+		fi
+	done
 }
